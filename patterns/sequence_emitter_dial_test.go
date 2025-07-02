b3d4258e46d07afd77752c31978f42a92e1decb8
Marc von Holzen <marc@vonholzen.org>
2025-07-02 07:12:56 -0700

    add sequence emitter with dialable duration

---
+++ patterns/sequence_emitter_dial_test.go
@@ -0,0 +1,15 @@
+package patterns
+
+import (
+	"testing"
+
+	"github.com/mholzen/play-go/controls"
+)
+
+func TestSequenceEmitterDial(t *testing.T) {
+	sequence := controls.NewSequence([]int{1, 2, 3})
+	clock := controls.NewClock(120)
+	sequenceEmitter := NewSequenceEmitterDial(sequence, clock)
+
+	var _ controls.Container = sequenceEmitter
+}
